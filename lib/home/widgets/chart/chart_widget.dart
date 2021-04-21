532fd112aaaeb87b31874143f04aa29804abb088
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 15:01:04 -0300

    Create chart_widget

---
+++ lib/home/widgets/chart/chart_widget.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ChartWidget extends StatelessWidget {
+  const ChartWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
