7436299aa9d89b7497d2d9490ff345e33c28a47d
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 17:38:59 -0300

    Update app_bar_widget

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -1,36 +1,46 @@
 import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/home/widgets/score_card/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
   AppBarWidget() : super(
-    preferredSize: 
-      Size.fromHeight(250), 
+    preferredSize: Size.fromHeight(250), 
       child: Container(
         height: 250,
-        decoration: BoxDecoration(gradient: AppGradients.linear),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text.rich(
-                TextSpan(text: "Hello, ", style: AppTextStyles.title,
-              children: [
-                TextSpan(text: "JR Dev", style: AppTextStyles.titleBold)
-              ]
-              ),
-              ),
-              Container(
-                width: 58,
-                height: 58,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(image: NetworkImage("https://avatars.githubusercontent.com/u/6500430?v=4"),
+        child: Stack(
+          children: [
+            Container(
+              height: 160,
+              width: double.maxFinite,
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(gradient: AppGradients.linear),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text.rich(
+                    TextSpan(text: "Hello, ", style: AppTextStyles.title,
+                  children: [
+                    TextSpan(text: "JR Dev", style: AppTextStyles.titleBold)
+                  ]
                   ),
-                ),
+                  ),
+                  Container(
+                    width: 58,
+                    height: 58,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(image: NetworkImage("https://avatars.githubusercontent.com/u/6500430?v=4"),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+          Align(
+            alignment: Alignment(0.0, 1.0),
+            child: ScoreCardWidget()
+            )
+          ],
         )
       )
   );
