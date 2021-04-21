d8a996295110efbe37afec64977035bbe0cdb438
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 17:38:27 -0300

    Update chart_widget

--- lib/home/widgets/chart/chart_widget.dart
+++ lib/home/widgets/chart/chart_widget.dart
@@ -1,4 +1,6 @@
 
+import 'package:dev_quiz/core/app_colors.dart';
+import 'package:dev_quiz/core/app_text_styles.dart';
 import 'package:flutter/material.dart';
 
 class ChartWidget extends StatelessWidget {
@@ -7,7 +9,29 @@ class ChartWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: child,
+      height: 80,
+      width: 80,
+      child: Stack(
+        children: [
+          Center(
+            child: Container(
+              height: 80,
+              width: 80,
+              child: CircularProgressIndicator(
+                strokeWidth: 10,
+                value: .75,
+                backgroundColor: AppColors.chartSecondary,
+                valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+              ),
+            ),
+          ),
+          Center(
+            child: Text(
+              "75%", style: AppTextStyles.heading,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
