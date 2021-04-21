e640921280c8e4691b1c3532031438868ed1b3db
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 14:36:36 -0300

    Create score_card_widget

---
+++ lib/home/widgets/score_card/score_card_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScoreCardWidget extends StatelessWidget {
+  const ScoreCardWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
