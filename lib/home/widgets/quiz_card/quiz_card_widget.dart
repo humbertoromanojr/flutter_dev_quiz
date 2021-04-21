f9086ba38bbe0f0cf95d418c6cc34d76ce4fa6ba
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 18:55:56 -0300

    Create quiz_card_widget

---
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuizCardWidget extends StatelessWidget {
+  const QuizCardWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
