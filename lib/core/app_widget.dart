b4e6172b8e2252d59f3987def5ac29cdee296aa9
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 12:41:47 -0300

    Linked app_bar_widget for home screen

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,4 @@
 import 'package:dev_quiz/home/home_page.dart';
-import 'package:dev_quiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
@@ -7,7 +6,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: SplashPage(),
+      home: HomePage(),
     );
   }
 }
