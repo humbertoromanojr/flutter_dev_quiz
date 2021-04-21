f6bd1599e06be9d0e9a71bfe26a0bcde80f08d70
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 13:31:29 -0300

    title and avatar Alignment

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -6,9 +6,32 @@ class AppBarWidget extends PreferredSize {
     preferredSize: 
       Size.fromHeight(250), 
       child: Container(
-        decoration: BoxDecoration(
-          gradient: AppGradients.linear
-        ),
+        height: 250,
+        decoration: BoxDecoration(gradient: AppGradients.linear),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text.rich(
+                TextSpan(text: "Hello, ", style: AppTextStyles.title,
+              children: [
+                TextSpan(text: "JR Dev", style: AppTextStyles.titleBold)
+              ]
+              ),
+              ),
+              Container(
+                width: 58,
+                height: 58,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(image: NetworkImage("https://avatars.githubusercontent.com/u/6500430?v=4"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )
       )
   );
 }
