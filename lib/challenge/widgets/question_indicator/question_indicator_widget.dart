80acad35aea48315c0450b6d479f363f1ee2a54d
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 21:59:18 -0300

    Refactoring progress_indicator_widget

--- lib/challenge/widgets/question_indicator/question_indicator_widget.dart
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -1,3 +1,5 @@
+import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuestionIndicatorWidget extends StatelessWidget {
@@ -6,7 +8,20 @@ class QuestionIndicatorWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Question 04", style: AppTextStyles.body),
+              Text("of 10", style: AppTextStyles.body)
+            ],
+          ),
+          SizedBox(height: 16),
+          ProgressIndicatorWidget(value: 0.7)
+        ],
+      ),
     );
   }
 }
--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/core/app_images.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -40,10 +41,8 @@ class QuizCardWidget extends StatelessWidget {
               ),
               Expanded(
                 flex: 4,
-                child: LinearProgressIndicator(
-                  value: 0.3,
-                  backgroundColor: AppColors.chartSecondary,
-                  valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                child: ProgressIndicatorWidget(
+                  value: 0.3
                 ),
               )
             ],
