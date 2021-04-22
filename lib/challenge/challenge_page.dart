9637cd35a5739a5cb0d7e573da5e88ed9ffe964b
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 21:05:15 -0300

    Create challenge_page

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatefulWidget {
+  ChallengePage({Key key}) : super(key: key);
+
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
