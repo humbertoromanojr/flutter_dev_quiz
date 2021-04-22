1143389b965c00c658bf96c08ffc448ca143f64b
humbertoromanojr <astrtonomi@gmail.com>
2021-04-21 21:11:02 -0300

    Create question_indicator_widget

---
+++ lib/challenge/widgets/question_indicator/question_indicator_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class QuestionIndicatorWidget extends StatelessWidget {
+  const QuestionIndicatorWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
