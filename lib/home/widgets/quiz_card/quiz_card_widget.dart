95763be5562a70863663327bb059ab2734c6dfbf
humbertoromanojr <astrtonomi@gmail.com>
2021-04-23 20:53:04 -0300

    Getting data come it controller

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -5,14 +5,25 @@ import 'package:dev_quiz/shared/widgets/progress_indicator/progress_indicator_wi
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
-  const QuizCardWidget({Key key}) : super(key: key);
+  final String title;
+  final String completed;
+  final double percent;
+
+  const QuizCardWidget({
+    Key key,
+    @required this.title,
+    @required this.completed, 
+    @required this.percent,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(10),
       decoration: BoxDecoration(
-        border: Border.fromBorderSide(BorderSide(color: AppColors.border),),
+        border: Border.fromBorderSide(
+          BorderSide(color: AppColors.border),
+        ),
         color: AppColors.white,
         borderRadius: BorderRadius.circular(10),
       ),
@@ -20,14 +31,11 @@ class QuizCardWidget extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
-            height: 40,
-            width: 40,
-            child: Image.asset(AppImages.blocks)
-          ),
+              height: 40, width: 40, child: Image.asset(AppImages.blocks)),
           SizedBox(height: 20),
           Text(
-            "State Management", 
-          style: AppTextStyles.heading15,
+            title,
+            style: AppTextStyles.heading15,
           ),
           SizedBox(height: 20),
           Row(
@@ -35,15 +43,13 @@ class QuizCardWidget extends StatelessWidget {
               Expanded(
                 flex: 1,
                 child: Text(
-                  "3/10",
+                  completed,
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
                 flex: 4,
-                child: ProgressIndicatorWidget(
-                  value: 0.3
-                ),
+                child: ProgressIndicatorWidget(value: percent),
               )
             ],
           ),
