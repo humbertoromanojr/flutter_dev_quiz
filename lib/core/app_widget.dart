538afbceccf0ca265d5c95e29b107aa2cd66f1dc
humbertoromanojr <astrtonomi@gmail.com>
2021-04-23 20:55:08 -0300

    Linked home_page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:dev_quiz/challenge/challenge_page.dart';
 import 'package:dev_quiz/home/home_page.dart';
 
 class AppWidget extends StatelessWidget {
@@ -8,7 +7,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: ChallengePage(),
+      home: HomePage(),
     );
   }
 }
