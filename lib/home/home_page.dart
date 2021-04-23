9cb089e9c0ee84dad357682892fb1aa6a7cb1389
humbertoromanojr <astrtonomi@gmail.com>
2021-04-23 20:49:34 -0300

    Adding loading before come data

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,9 +1,11 @@
+import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:dev_quiz/home/widgets/level_button/level_button_widget.dart';
 import 'package:dev_quiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 import 'home_contrtoller.dart';
+import 'home_state.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -16,46 +18,70 @@ class _HomePageState extends State<HomePage> {
   final controller = HomeController();
 
   @override
+  void initState() {
+    super.initState();
+
+    controller.getUser();
+    controller.getQuizzes();
+
+    controller.stateNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBarWidget(),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(height: 24),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  LevelButtonWidget(
-                    labelLevel: "Easy",
-                  ),
-                  LevelButtonWidget(
-                    labelLevel: "Average",
-                  ),
-                  LevelButtonWidget(
-                    labelLevel: "Difficult",
-                  ),
-                  LevelButtonWidget(
-                    labelLevel: "Expert",
-                  ),
-                ],
-              ),
-              SizedBox(height: 24),
-              Expanded(
-                child: GridView.count(
-                  crossAxisSpacing: 16,
-                  mainAxisSpacing: 16,
-                  crossAxisCount: 2,
+    if (controller.state == HomeState.success) {
+      return Scaffold(
+          appBar: AppBarWidget(user: controller.user),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    QuizCardWidget(),
-                    QuizCardWidget(),
-                    QuizCardWidget(),
+                    LevelButtonWidget(
+                      labelLevel: "Easy",
+                    ),
+                    LevelButtonWidget(
+                      labelLevel: "Average",
+                    ),
+                    LevelButtonWidget(
+                      labelLevel: "Difficult",
+                    ),
+                    LevelButtonWidget(
+                      labelLevel: "Expert",
+                    ),
                   ],
                 ),
-              ),
-            ],
-          ),
-        ));
+                SizedBox(height: 24),
+                Expanded(
+                  child: GridView.count(
+                      crossAxisSpacing: 16,
+                      mainAxisSpacing: 16,
+                      crossAxisCount: 2,
+                      children: controller.quizzes
+                          .map((e) => QuizCardWidget(
+                                title: e.title,
+                                percent:
+                                    e.questionAnswered / e.questions.length,
+                                completed:
+                                    "${e.questionAnswered}/${e.questions.length}",
+                              ))
+                          .toList()),
+                ),
+              ],
+            ),
+          ));
+    } else {
+      return Scaffold(
+        body: Center(
+            child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(AppColors.darkGreen),
+        )),
+      );
+    }
   }
 }
