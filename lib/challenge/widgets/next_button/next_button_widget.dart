61b55ff2c3164c463c0f8fdf0d369328b3f54ef6
humbertoromanojr <astrtonomi@gmail.com>
2021-04-24 10:45:17 -0300

    Create next_button_widget

---
+++ lib/challenge/widgets/next_button/next_button_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NextbuttonWidget extends StatelessWidget {
+  const NextbuttonWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
