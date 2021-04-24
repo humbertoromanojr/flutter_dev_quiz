0b309e636044a38baab69f38524e5e694bd9aee6
humbertoromanojr <astrtonomi@gmail.com>
2021-04-24 10:59:17 -0300

    Refactoring getting data home_contrtoller

--- lib/home/home_contrtoller.dart
+++ lib/home/home_contrtoller.dart
@@ -1,10 +1,9 @@
-import 'package:dev_quiz/core/app_images.dart';
 import 'package:dev_quiz/home/home_state.dart';
-import 'package:dev_quiz/shared/models/awnser_model.dart';
-import 'package:dev_quiz/shared/models/question_model.dart';
 import 'package:dev_quiz/shared/models/quiz_model.dart';
 import 'package:dev_quiz/shared/models/user_model.dart';
 import 'package:flutter/foundation.dart';
+
+import 'home_repository.dart';
 
 class HomeController {
   // state management
@@ -15,15 +14,12 @@ class HomeController {
   UserModel user;
   List<QuizModel> quizzes;
 
+  final repository = HomeRepository();
+
   void getUser() async {
     state = HomeState.loading;
 
-    await Future.delayed(Duration(seconds: 2));
-
-    user = UserModel(
-      name: "JR DEV",
-      photoUrl: "https://avatars.githubusercontent.com/u/6500430?v=4",
-    );
+    user = await repository.getUser();
 
     state = HomeState.success;
   }
@@ -31,31 +27,7 @@ class HomeController {
   void getQuizzes() async {
     state = HomeState.loading;
 
-    await Future.delayed(Duration(seconds: 2));
-
-    quizzes = [
-      QuizModel(
-        title: "NLW 05 FLUTTER",
-        imagem: AppImages.blocks,
-        level: Level.facil,
-        questions: [
-          QuestionModel(title: "Está gostando de aprender Flutter", awnsers: [
-            AwnserModel(title: "Estou curtindo abeça"),
-            AwnserModel(title: "Estou curtindo demais"),
-            AwnserModel(title: "Estou curtindo muito", isRight: true),
-            AwnserModel(title: "Estou curtindo pra caramba"),
-          ]),
-          QuestionModel(
-              title: "O que gostou mais de aprender Flutter",
-              awnsers: [
-                AwnserModel(title: "Acaffold"),
-                AwnserModel(title: "Contrtuir a UI", isRight: true),
-                AwnserModel(title: "Gerenciar estado"),
-                AwnserModel(title: "Animações básicas"),
-              ]),
-        ],
-      )
-    ];
+    quizzes = await repository.getQuizzes();
 
     state = HomeState.success;
   }
