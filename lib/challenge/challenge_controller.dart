5ad5ad608fbc91c0e1263222a52dc100c8f502d7
humbertoromanojr <astronomi@gmail.com>
2021-04-24 16:18:44 -0300

    Reactive controller

--- lib/challenge/challenge_controller.dart
+++ lib/challenge/challenge_controller.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/foundation.dart';
+
 class ChallengeController {
-  var currentpage = 0;
+  final currentPageNotifier = ValueNotifier<int>(0);
+  int get currentPage => currentPageNotifier.value;
+  set currentPage(int value) => currentPageNotifier.value = value;
 }
