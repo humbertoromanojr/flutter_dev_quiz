be840f6bd425153dcb480108c1be7045de6f8dc6
humbertoromanojr <astronomi@gmail.com>
2021-04-24 20:32:51 -0300

    Converting in stateful class

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -3,20 +3,25 @@ import 'package:dev_quiz/core/app_text_styles.dart';
 import 'package:dev_quiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
+class QuizWidget extends StatefulWidget {
   final QuestionModel question;
 
   const QuizWidget({Key key, @required this.question}) : super(key: key);
 
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
           SizedBox(height: 64),
-          Text(question.title, style: AppTextStyles.heading),
+          Text(widget.question.title, style: AppTextStyles.heading),
           SizedBox(height: 24),
-          ...question.awnsers.map(
+          ...widget.question.awnsers.map(
             (e) => AwnserWidget(
               isRight: e.isRight,
               title: e.title
