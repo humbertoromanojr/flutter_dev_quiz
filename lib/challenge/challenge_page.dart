220d2435e0a5abb15a0efa4b5eca3dd6db088db6
humbertoromanojr <astronomi@gmail.com>
2021-04-24 16:27:17 -0300

    Get list questions

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -19,6 +19,15 @@ class _ChallengePageState extends State<ChallengePage> {
   final controller = ChallengeController();
 
   @override
+  void initState() {
+    super.initState();
+
+    controller.currentPageNotifier.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
@@ -37,8 +46,8 @@ class _ChallengePageState extends State<ChallengePage> {
           ),
         ),
       ),
-      body: QuizWidget(
-        question: widget.questions[0],
+      body: PageView(
+        children: widget.questions.map((e) => QuizWidget(question: e)).toList(),
       ),
       bottomNavigationBar: SafeArea(
         bottom: true,
