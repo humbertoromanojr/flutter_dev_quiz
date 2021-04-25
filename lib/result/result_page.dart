9b7713f6ff06dcd0f384c56eab58e92202540a2e
humbertoromanojr <astronomi@gmail.com>
2021-04-25 13:19:57 -0300

    Alignment

--- lib/result/result_page.dart
+++ lib/result/result_page.dart
@@ -1,4 +1,6 @@
+import 'package:dev_quiz/challenge/widgets/next_button/next_button_widget.dart';
 import 'package:dev_quiz/core/app_text_styles.dart';
+import 'package:dev_quiz/core/core.dart';
 import 'package:flutter/material.dart';
 
 class ResultPage extends StatelessWidget {
@@ -9,13 +11,56 @@ class ResultPage extends StatelessWidget {
     return Scaffold(
       body: Container(
         width: double.maxFinite,
+        padding: EdgeInsets.only(top: 100),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Parabéns", style: AppTextStyles.heading40),
-            Text("Parabéns", style: AppTextStyles.heading40),
-            Text("Parabéns", style: AppTextStyles.heading40),
+            Image.asset(AppImages.trophy),
+            Column(
+              children: [
+                Text("Parabéns!", style: AppTextStyles.heading40),
+                SizedBox(height: 16),
+                Text.rich(
+                  TextSpan(
+                      text: "Você concluiu ",
+                      style: AppTextStyles.body,
+                      children: [
+                        TextSpan(
+                            text: "\nGerenciamento de Estado ",
+                            style: AppTextStyles.bodyBold),
+                        TextSpan(
+                            text: "\ncom 6/10 acertos.", style: AppTextStyles.body),
+                      ]),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+            Column(children: [
+              Row(
+              children: [
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 68),
+                  child: NextButtonWidget.purple(
+                      label: "Compartilhar", onTap: () {}),
+                )),
+              ],
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 68),
+                  child: NextButtonWidget.transparent(
+                      label: "Voltar ao início",
+                      onTap: () {
+                        Navigator.pop(context);
+                      }),
+                ),
+              ],
+            ),
+            ],)
           ],
         ),
       ),
