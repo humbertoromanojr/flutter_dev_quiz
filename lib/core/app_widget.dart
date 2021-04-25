f6178d51ff4391153644207c82fa25d9f8211f65
humbertoromanojr <astronomi@gmail.com>
2021-04-24 22:56:51 -0300

    Configuring begin splash_page

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:dev_quiz/home/home_page.dart';
+import 'package:dev_quiz/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: SplashPage(),
     );
   }
 }
--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,9 +1,16 @@
 import 'package:dev_quiz/core/core.dart';
+import 'package:dev_quiz/home/home_page.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    Future.delayed(Duration(seconds: 3))
+      .then((_) => Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomePage()),
+    ));
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
