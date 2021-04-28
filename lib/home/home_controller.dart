0c8a6afa1d5978d31663e6a37136b3bd029fe101
humbertoromanojr <astronomi@gmail.com>
2021-04-27 23:16:10 -0300

    Delete and recreate home_controller

--- lib/home/home_controller.dart
+++ lib/home/home_controller.dart
@@ -1,34 +1,34 @@
-import 'package:dev_quiz/home/home_state.dart';
-import 'package:dev_quiz/shared/models/quiz_model.dart';
-import 'package:dev_quiz/shared/models/user_model.dart';
 import 'package:flutter/foundation.dart';
 
+import 'package:dev_quiz/shared/models/quiz_model.dart';
+import 'package:dev_quiz/shared/models/user_model.dart';
+
+import 'home_state.dart';
 import 'home_repository.dart';
 
 class HomeController {
   // state management
-  final stateNotifier = ValueNotifier<HomeState>(HomeState.empty);
+  ValueNotifier<HomeState> stateNotifier =
+      ValueNotifier<HomeState>(HomeState.empty);
   set state(HomeState state) => stateNotifier.value = state;
   HomeState get state => stateNotifier.value;
 
-  UserModel user;
-  List<QuizModel> quizzes;
+  UserModel? user;
+  List<QuizModel>? quizzes;
 
   final repository = HomeRepository();
 
   void getUser() async {
     state = HomeState.loading;
-
+    await Future.delayed(Duration(seconds: 2));
     user = await repository.getUser();
-
     state = HomeState.success;
   }
 
   void getQuizzes() async {
     state = HomeState.loading;
-
+    await Future.delayed(Duration(seconds: 2));
     quizzes = await repository.getQuizzes();
-
     state = HomeState.success;
   }
 }
